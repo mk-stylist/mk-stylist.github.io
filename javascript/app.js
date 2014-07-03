0b6902c1e4c8bcac4244cfdec4f52d08c184e68b
Yann Marquet <ymarquet@gmail.com>
2014-07-03 22:22:02 +0100

    first blood

---
+++ javascript/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:               true,
+    autoplay:           true,
+    autoplayTimeout:    2000,
+    autoplayHoverPause: true,
+    animateOut:         'fadeOutUp',
+    dots:               true,
+    smartSpeed:         450,
+    items:              1
+  });
+});
